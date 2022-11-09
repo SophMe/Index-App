bae3da6070dfc575bd0e48f3fc826d8baac18d1e
SophMe <116891325+SophMe@users.noreply.github.com>
2022-11-09 14:10:21 +0100

    add for loop and if conditional within it

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 //hitpoints is a number
 let pokemonList = [
   {name: "Bulbasaur", 
-  height: 7, 
+  height: 0.7, 
   types:['grass', 'poison'],
   hitpoints: 45},
   
@@ -24,7 +24,18 @@ let pokemonList = [
   hitpoints: 65}
   ];
 
-//Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property name
- for(let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + '</p>')
+//Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
+//  for(let i = 0; i < pokemonList.length; i++){
+//     document.write('<p>' + pokemonList[i].name + '</p>')
+//     }
+
+//Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
+// if height is higher than 1.5 write name , height and "Wow, that's big!", else write name and height
+  for(let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 1.5) {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ ' - Wow, that\'s big!' + '</p>') 
+    } else {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ '</p>') /*put style as string into index.html*/
     }
+  } 
+    
