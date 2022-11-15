9b08fc749487692400bb1ca6b9978bc93469f121
SophMe <116891325+SophMe@users.noreply.github.com>
2022-11-15 10:56:45 +0100

    add IIFE and forEach

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
+//forEach and IIFE
+//next line is IIFE function, with and array (pokemonList) in the curly brackets 
+//let pokemonRepository = (function () {
+
 //name is a string
 //height is a number
 //types is an array of strings
 //hitpoints is a number
-let pokemonList = [
-  {name: "Bulbasaur", 
-  height: 0.7, 
-  types:['grass', 'poison'],
-  hitpoints: 45},
-  
-  {name: "Pikachu", 
-  height: 0.4, 
-  types:'electric',
-  hitpoints: 35},
-  
-  {name: "Charizad", 
-  height: 1.7, 
-  types:['fire', 'flying'],
-  hitpoints: 78},
- 
- {name: "Beedrill", 
-  height: 1, 
-  types:['bug', 'poison'],
-  hitpoints: 65}
-  ];
+  let pokemonList = [
+    {name: "Bulbasaur", 
+    height: 0.7, 
+    types:['grass', 'poison'],
+    hitpoints: 45},
+    
+    {name: "Pikachu", 
+    height: 0.4, 
+    types:'electric',
+    hitpoints: 35},
+    
+    {name: "Charizad", 
+    height: 1.7, 
+    types:['fire', 'flying'],
+    hitpoints: 78},
+   
+   {name: "Beedrill", 
+    height: 1, 
+    types:['bug', 'poison'],
+    hitpoints: 65}
+    ];
+
+//"The IIFE returns an object with two keys: add and getAll. This means that, whenever you access pokemonRepository somewhere in your app, it will represent an object with these two keys."??
+//Two seperate functions
+/*  function getAll () {
+    return pokemonList;
+  }
+
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+//Closing brackets of IIFE
+}
+  ) ();*/
 
 //Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
 //  for(let i = 0; i < pokemonList.length; i++){
@@ -31,27 +54,22 @@ let pokemonList = [
 
 //Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
 // if height is higher than 1.5 write name , height and "Wow, that's big!", else write name and height
-  for(let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 1.5) {
-      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ ' - Wow, that\'s big!' + '</p>') 
-    } else {
-      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ '</p>') /*put style as string into index.html*/
-    }
-  } 
-    
-function divide(dividend, divisor){
-  if (divisor === 0) {
-    return("You\'re trying to divide by zero")
-  }
-  else {
-    let result =dividend/divisor;
-    return result;
-  }
-}
+//  for(let i = 0; i < pokemonList.length; i++){
+//  if (pokemonList[i].height > 1.5) {
+//      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ ' - Wow, that\'s big!' + '</p>') 
+//    } else {
+//      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ '</p>') /*put style as string into index.html*/
+//    }
+//  } 
 
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
+//forEach
+//my array.forEach(function(property){action(property)}); ??!
+pokemonList.forEach(function(name){
+  document.write('<p>' + pokemonList.name + '</p>');
+});
+
+
+
+
 
 
