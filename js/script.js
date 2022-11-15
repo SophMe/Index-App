a79058242d2780b5d46f08fd3fa2e3871bb54f7d
SophMe <116891325+SophMe@users.noreply.github.com>
2022-11-15 09:37:01 +0100

    remove practice code

--- js/script.js
+++ js/script.js
@@ -39,3 +39,19 @@ let pokemonList = [
     }
   } 
     
+function divide(dividend, divisor){
+  if (divisor === 0) {
+    return("You\'re trying to divide by zero")
+  }
+  else {
+    let result =dividend/divisor;
+    return result;
+  }
+}
+
+console.log(divide(4, 2));
+console.log(divide(7, 0));
+console.log(divide(1, 4));
+console.log(divide(12, -3));
+
+
