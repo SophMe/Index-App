6afb2ca3c0485ea1d6f663f6c111d7c698bdf6a7
SophMe <116891325+SophMe@users.noreply.github.com>
2022-11-16 12:04:29 +0100

    add IIFE and forEach

--- js/script.js
+++ js/script.js
@@ -49,30 +49,20 @@ let pokemonRepository = (function () {
 }
   ) ();
 
-//Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
-//  for(let i = 0; i < pokemonList.length; i++){
-//     document.write('<p>' + pokemonList[i].name + '</p>')
-//     }
-
-//Loop(iterator i starts with index 0; as long as i is less than number of objects; add 1 to i) -> write property "name"
-// if height is higher than 1.5 write name , height and "Wow, that's big!", else write name and height
-//  for(let i = 0; i < pokemonList.length; i++){
-//  if (pokemonList[i].height > 1.5) {
-//      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ ' - Wow, that\'s big!' + '</p>') 
-//    } else {
-//      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)'+ '</p>') /*put style as string into index.html*/
-//    }
-//  } 
-
 //forEach
-//The function(item) is passed as a parameter to the forEach()function
+//console.log(pokemonRepository.getAll());
+//document.write(pokemonRepository.getAll());
+//The function(pokemon) is passed as a parameter to the forEach()function
 //my array.forEach(function(property){action(key.property)}); 
-/*pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name);
+//pokemonList.forEach(function(pokemon) {
+//  document.write(pokemon.name);
+//});
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  console.log("pokemon:", pokemon.name)
 });
-*/
-console.log(pokemonRepository.getAll());
-document.write(pokemonRepository.getAll());
+
+
 
 
 
